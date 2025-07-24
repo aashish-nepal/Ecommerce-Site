c55b8a9d58955b82d06b9126dbd731a705c5f53c
Aashish Nepal <nepal.aashish00@gmail.com>
2025-07-24 21:49:58 +0545

    Fixed Deployment-Errors

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
-import Header from "./components/Header/Header";
-import HeroCarousel from "./components/HeroCarousel/HeroCarousel";
-import BannerGrid from "./components/BannerGrid/BannerGrid";
-import CategoryNav from "./components/CategoryNav/CategoryNav";
-import ProductGrid from "./components/ProductGrid/ProductGrid";
-import CategorySection from "./components/CategorySection/CategorySection";
+import Header from "../../components/Header/Header";
+import HeroCarousel from "../../components/HeroCarousel/HeroCarousel";
+import BannerGrid from "../../components/BannerGrid/BannerGrid";
+import CategoryNav from "../../components/CategoryNav/CategoryNav";
+import ProductGrid from "../../components/ProductGrid/ProductGrid";
+import CategorySection from "../../components/CategorySection/CategorySection";
 
 
 export default function Home() {
